fd677020afa7c7c3b1aa0f2c1d19c4168d14a517
ayush1470 <ayush170203@gmail.com>
2025-03-26 00:11:19 +0530

    added inbuilt substring function

--- Substring/src/Main.java
+++ Substring/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static String Substring(String str, int si, int ei){
+    public static String Substring(String str, int si, int ei){  // si -> starting index, ei -> ending index (excluded)
         String Substr = "";
         for(int i = si; i<ei; i++){
             Substr += str.charAt(i);
@@ -10,5 +10,6 @@ public class Main {
     public static void main(String[] args) {
         String str = "HelloWorld";
         System.out.println(Substring(str,0,4));
+        System.out.println(str.substring(0,5));  // inbuilt function to make the substring of any string
     }
 }
